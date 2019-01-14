bbfce1cc1900b6c4c30bbcabe465cd4659b0609b
Seva Zaikov <seva.zaikov@gmail.com>
2019-01-13 20:39:15 -0800

    switch to webpack

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-const path = require("path");
-
-require("@babel/register")({
-  presets: [
-    [
-      // path.resolve(__dirname, "..", "node_modules", "@babel", "preset-react"),
-      "@babel/preset-react",
-      {
-        pragma: "Welgo.createElement",
-        pragmaFrag: "Welgo.Fragment"
-      }
-    ]
-  ]
-});
-
 require("./app");
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+  mode: isProduction ? "production" : "development",
+  watch: !isProduction,
+  target: "node",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "src"),
+    filename: "server.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-react",
+              {
+                pragma: "Welgo.createElement",
+                pragmaFrag: "Welgo.Fragment"
+              }
+            ]
+          ]
+        }
+      },
+      {
+        test: /.sass$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]",
+          {
+            loader: "sass-loader",
+            options: {
+              indentedSyntax: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "public/main.css"
+    })
+  ],
+  // from here – https://codeburst.io/use-webpack-with-dirname-correctly-4cad3b265a92
+  // docs – https://webpack.js.org/configuration/node/#node-__dirname
+  node: {
+    __dirname: false
+  }
+};
